ad5afcc061374d489163aa25705d0a8acea22587
macuguita <raulpripri@gmail.com>
2025-07-26 00:32:42 +0200

    updated to 1.21.8

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -1,6 +1,6 @@
 object BuildConfig {
-    val minecraftVersion: String = "1.21.6"
-    val minecraftVersionRange: String = ">=1.21.6"
+    val minecraftVersion: String = "1.21.8"
+    val minecraftVersionRange: String = ">=1.21.6 <=1.21.8"
     val yarnMappings: String = minecraftVersion + "+build.1"
     val loaderVersion: String = "0.16.14"
 
@@ -11,7 +11,7 @@ object BuildConfig {
     val description: String = "An addon for Farmer's Delight Refabricated that adds regional plates from Spain!"
     val license: String = "MIT"
 
-    val fabricVersion: String = "0.128.2+" + minecraftVersion
+    val fabricVersion: String = "0.129.0+" + minecraftVersion
     val modMenuVersion: String = "15.0.0-beta.3"
-    val fdrfVersion: String = minecraftVersion + "-3.2.3+refabricated"
+    val fdrfVersion: String = minecraftVersion + "-3.2.4+refabricated"
 }
