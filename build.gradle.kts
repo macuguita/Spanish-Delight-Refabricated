3b4547b863a51d19513256444b5e97555c772ff2
macuguita <raulpripri@gmail.com>
2025-11-24 16:35:46 +0100

    gradle changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("fabric-loom").version("1.9-SNAPSHOT")
     id("maven-publish")
+    id("me.modmuss50.mod-publish-plugin").version("1.0.0")
 }
 
 loom {
@@ -41,9 +42,16 @@ base {
 repositories {
     maven {
         name = "Greenhouse Maven"
-        url = uri("https://repo.greenhouse.house/releases/")
+        url = uri("https://maven.greenhouse.lgbt/releases/")
     }
-    maven { url = uri("https://mvn.devos.one/releases/") } // Porting Lib
+    maven {
+        name = "Greenhouse Maven"
+        url = uri("https://maven.greenhouse.lgbt/snapshots/") // Porting Lib Hotfixes
+    }
+    maven {
+        name = "Porting Lib"
+        url = uri("https://mvn.devos.one/releases/")
+    }
     maven {
         url = uri("https://maven.jamieswhiteshirt.com/libs-release") // Reach Entity Attributes (Required by Porting Lib)
         content {
@@ -77,6 +85,32 @@ dependencies {
 
     modImplementation("vectorwing:FarmersDelight:${BuildConfig.fdrfVersion}") {
         exclude(group = "net.fabricmc")
+    }
+}
+
+val changelogText: String = File("CHANGELOG.md").readText()
+
+publishMods {
+    changelog = changelogText
+    file.set(tasks.remapJar.get().archiveFile)
+    additionalFiles.from(tasks.remapSourcesJar.get().archiveFile)
+    displayName = BuildConfig.modName + " " + BuildConfig.modVersion
+    version = BuildConfig.modVersion
+    if (BuildConfig.modVersion.contains("beta")) {
+        type = BETA
+    } else {
+        type = STABLE
+    }
+    modLoaders.add("fabric")
+    modLoaders.add("quilt")
+    dryRun = providers.environmentVariable("MODRINTH_TOKEN").getOrNull() == null
+    modrinth {
+        projectId = "E2LV3K2B"
+        accessToken = providers.environmentVariable("MODRINTH_TOKEN")
+        for (version in BuildConfig.supportedVersions)
+            minecraftVersions.add(version)
+        requires("fabric-api")
+        requires("farmers-delight-refabricated")
     }
 }
 
