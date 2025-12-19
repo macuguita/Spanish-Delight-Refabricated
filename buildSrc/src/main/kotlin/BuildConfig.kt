e9e32a673fdd3fca09bfcbd355765cb66acad7c2
macuguita <raulpripri@gmail.com>
2025-12-19 03:12:18 +0100

    version bump

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,7 +5,7 @@ object BuildConfig {
     val yarnMappings: String = minecraftVersion + "+build.3"
     val loaderVersion: String = "0.16.14"
 
-    val modVersion: String = minecraftVersion + "-1.0.7"
+    val modVersion: String = minecraftVersion + "-1.0.8a"
     val mavenGroup: String = "io.github.macuguita"
     val modId: String = "spanishdelight"
     val modName: String = "Spanish Delight Refabricated"
